65131a66c6d9cd854e7975cbaf3cba0dd9e9d348
Chip Castle <chip@chipcastle.com>
2015-04-11 13:20:57 -0500

    Update github link to pages site

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'chipcastledotcom:jspdf-autotable',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'Meteor package for AutoTable plugin to jsPDF',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/ChipCastleDotCom/meteor-jspdf-autotable.git',
+  git: 'http://chipcastledotcom.github.io/meteor-jspdf-autotable',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
